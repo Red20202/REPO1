ebc0c7344ca68a04e0743cae4e34d034f9b9ae9c
Red20202 <96047573+Red20202@users.noreply.github.com>
2022-02-10 11:36:26 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("HI");
+var firstname="Hello";
+console.log(firstname);
+var lastname="World";
+console.log(lastname);
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+
+var length=fullname.length;
+console.log(length);
+
+var name_one="Hello mum";
+console.log(name_one);
+var replace_name_one=name_one.replace("mum","ARJUNBOIS");
+console.log(replace_name_one);
+
+var capitalname="HELLOOOWORLD";
+var small=capitalname.toLowerCase();
+console.log(small)
